c289436a71881e68925e90c2d8ee2c0bdcab547a
junwon1997 <junwon20170339@gmail.com>
2023-08-21 17:11:53 +0900

    add: 프로젝트 구조 생성

---
+++ daitso/src/main/java/com/example/daitso/DaitsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.daitso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaitsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaitsoApplication.class, args);
+	}
+
+}
---
+++ daitso/src/test/java/com/example/daitso/DaitsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daitso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaitsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
