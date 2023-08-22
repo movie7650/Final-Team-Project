c7afd7952a6169f4d0b4159e8ccb046ac39fb48d
ntweety <ntweety@naver.com>
2023-08-22 15:04:38 +0900

    add: Point, Product, Purchase, Review, Shipping model 필드 생성

--- daitso/src/main/java/com/example/daitso/point/model/Point.java
+++ daitso/src/main/java/com/example/daitso/point/model/Point.java
@@ -1,5 +1,21 @@
 package com.example.daitso.point.model;
 
-public class Point {
+import java.sql.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Point {
+	private int pointId;
+	private int customerId;
+	private int pointBefore;
+	private int pointAfter;
+	private String pointUpdateContent;
+	private int pointUseDv;
+	private String status;
+	private Date createDt;
+	private String creator;
+	private Date modifyDt;
+	private String modifier;
 }
--- daitso/src/main/java/com/example/daitso/product/model/Product.java
+++ daitso/src/main/java/com/example/daitso/product/model/Product.java
@@ -1,5 +1,31 @@
 package com.example.daitso.product.model;
 
-public class Product {
 
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+	private int productId;
+	private int categoryId;
+	private String productNm;
+	private String productContent;
+	private int productPrice;
+	private int productStock;
+	private int productSalesCount;
+	private String productOptionFirst;
+	private String productOptionSecond;
+	private String productOptionThird;
+	private String productImageFirst;
+	private String productImageSecond;
+	private String productImageThird;
+	private String status;
+	private Date create_dt;
+	private String creator;
+	private Date modify_dt;
+	private String modifier;
+	private String productCode;
 }
