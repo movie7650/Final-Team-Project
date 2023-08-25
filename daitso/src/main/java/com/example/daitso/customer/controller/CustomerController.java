bf0e8c2136a2b88657f572bc9047e6aa84a121a7
junwon1997 <junwon20170339@gmail.com>
2023-08-25 11:28:15 +0900

    modify: [준원] 부트스트랩 전용 header 파일 생성 및 기존 header 수정

--- daitso/src/main/java/com/example/daitso/customer/controller/CustomerController.java
+++ daitso/src/main/java/com/example/daitso/customer/controller/CustomerController.java
@@ -1,5 +1,23 @@
 package com.example.daitso.customer.controller;
 
-public class CustomerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.daitso.customer.model.CustomerSignUp;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@GetMapping("/sign-up")
+	public String signUpGet(CustomerSignUp customerSignUp) {
+		return "sign-up/sign-up";
+	}
+	
+	@PostMapping("/sign-up")
+	public String signUpPost(CustomerSignUp customerSignUp) {
+		return "";
+	}
 }
