4e27a33c0ad3623c9f861bc7a34886555260119c
movie7650 <movie7650@naver.com>
2023-08-28 16:59:23 +0900

    add:[상영] 리뷰관리-리뷰작성 프론트 완료

--- daitso/src/main/java/com/example/daitso/review/controller/ReviewController.java
+++ daitso/src/main/java/com/example/daitso/review/controller/ReviewController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.daitso.review.service.IReviewService;
 
 @Controller
+@RequestMapping("/mypage")
 public class ReviewController {
 	
 	@Autowired
 	IReviewService reviewService;
-	
+	 //리뷰 페이지 불러오기
 	@RequestMapping("/review")
 	public String header() {
 		return "mypage/review";
