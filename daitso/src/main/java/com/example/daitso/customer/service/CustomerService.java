236ba8719079be2899f938fc95ab2d0dc4571ca2
junwon1997 <junwon20170339@gmail.com>
2023-08-29 10:00:39 +0900

    add:[준원] 회원가입 기능 완료

--- daitso/src/main/java/com/example/daitso/customer/service/CustomerService.java
+++ daitso/src/main/java/com/example/daitso/customer/service/CustomerService.java
@@ -1,5 +1,44 @@
 package com.example.daitso.customer.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.daitso.customer.model.CustomerSignUp;
+import com.example.daitso.customer.repository.ICustomerRepository;
+
+@Service
 public class CustomerService implements ICustomerService {
 
+	@Autowired
+	ICustomerRepository customerRepository;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	// 이메일 중복체크
+	@Override
+	public Optional<Integer> checkEmailDuplicated(String customerEmail) {
+		return customerRepository.checkEmailDuplicated(customerEmail);
+	}
+	
+	// 휴대폰 중복체크
+	@Override
+	public Optional<Integer> checkTelnoDuplicated(String customerTelno) {
+		return customerRepository.checkTelnoDuplicated(customerTelno);
+	}
+
+	// 회원가입
+	@Transactional
+	public void insertIntoCustomer(CustomerSignUp customerSignUp){
+		
+		// 비밀번호 암호화
+		String encryptedPassword = passwordEncoder.encode(customerSignUp.getCustomerPw());
+		customerSignUp.setCustomerPw(encryptedPassword);
+		
+		customerRepository.insertIntoCustomer(customerSignUp);
+	}	
 }
