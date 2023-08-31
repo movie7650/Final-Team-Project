33b13dcafa51134b6b58276a62debce3081e793c
junwon1997 <junwon20170339@gmail.com>
2023-08-31 11:00:51 +0900

    add:[준원] 로그인 성공시 관리자,사용자 화면 다르게

--- daitso/src/main/java/com/example/daitso/admin/controller/AdminController.java
+++ daitso/src/main/java/com/example/daitso/admin/controller/AdminController.java
@@ -1,16 +1,11 @@
 package com.example.daitso.admin.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-	// 로그인
-	@GetMapping("/login")
-	public String login() {
-		return "admin/login/admin-login";
-	}
+	
 }
