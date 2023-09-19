96e7cad025d27b474bc4c571d8c5f76b521e1711
movie7650 <movie7650@naver.com>
2023-09-19 20:37:14 +0900

    modify:[상영] 주문조회 입금/결제 배송중 배송완료 상품 없을 때 예외처리 완료

--- daitso/src/main/java/com/example/daitso/purchase/repository/IPurchaseRepository.java
+++ daitso/src/main/java/com/example/daitso/purchase/repository/IPurchaseRepository.java
@@ -36,6 +36,9 @@ public interface IPurchaseRepository {
 	//배송완료인 상품 갯수
 	int selectShippingComplete(int customerId);
 	
+	//입금/결제상품갯수
+	int selectPayCoin(int customerId);
+	
 	//상세주문조회
 	List<PurchaseDetailCheck> selectDetailPurchase(@Param("customerId") int customerId, @Param("purchaseNum")String purchaseNum);
 
