b6f5ca3e6682e84715254e0c4b57ab3d0e6b525c
ntweety <ntweety@naver.com>
2023-10-26 10:36:06 +0900

    modify : [현웅] web api stt로 변경

---
+++ daitso/src/main/java/com/example/daitso/config/Recongize.java
@@ -0,0 +1,52 @@
+package com.example.daitso.config;
+
+
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+
+public class Recongize {
+	/**
+	 * Performs speech recognition on raw PCM audio and prints the transcription.
+	 *
+	 * @param fileName the path to a PCM audio file to transcribe.
+	 */
+	public static void syncRecognizeFile(String fileName) throws Exception {
+	  try (SpeechClient speech = SpeechClient.create()) {
+	    Path path = Paths.get(fileName);
+	    byte[] data = Files.readAllBytes(path);
+	    ByteString audioBytes = ByteString.copyFrom(data);
+
+	    // Configure request with local raw PCM audio
+	    RecognitionConfig config =
+	        RecognitionConfig.newBuilder()
+	            .setEncoding(AudioEncoding.LINEAR16)
+	            .setLanguageCode("ko-KR")
+	            .setSampleRateHertz(16000)
+	            .build();
+	    RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+	    // Use blocking call to get audio transcript
+	    RecognizeResponse response = speech.recognize(config, audio);
+	    List<SpeechRecognitionResult> results = response.getResultsList();
+
+	    for (SpeechRecognitionResult result : results) {
+	      // There can be several alternative transcripts for a given chunk of speech. Just use the
+	      // first (most likely) one here.
+	      SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+	      System.out.printf("Transcription: %s%n", alternative.getTranscript());
+	    }
+	  }
+	}
+}
